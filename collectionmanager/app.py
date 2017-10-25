854a9656568ad046a31a9f1c8c2ebcc2e51ae2e1
Kostas Kokkoros <mavroprovato@gmail.com>
2017-10-26 01:37:09 +0300

    Add documentation.

--- collectionmanager/app.py
+++ collectionmanager/app.py
@@ -1,3 +1,6 @@
+"""
+The application module
+"""
 import PyQt5.QtWidgets as QtWidgets
 import sys
 
@@ -5,14 +8,26 @@ import collectionmanager.ui.main_window as main_window
 
 
 class CollectionManagerApp(QtWidgets.QMainWindow, main_window.Ui_MainWindow):
+    """
+    The application class
+    """
     def __init__(self, parent=None):
+        """Constructor for the main application class.
+
+        :param parent: The parent window.
+        """
         super(CollectionManagerApp, self).__init__(parent)
 
         self.setupUi(self)
+
+        # Connect the application actions
         self.actionQuit.triggered.connect(QtWidgets.qApp.quit)
 
 
 def main():
+    """
+    The main entry point of the application.
+    """
     app = QtWidgets.QApplication(sys.argv)
     form = CollectionManagerApp()
     form.show()
