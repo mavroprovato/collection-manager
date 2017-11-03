38ea37893d183d17765dab7e180b4b5cc85388d8
Kostas Kokkoros <k.kokkoros@intelen.com>
2017-11-03 15:28:07 +0200

    Create a separate UI file for the central widget

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 UI_DIR = collectionmanager/ui
 
-all: $(UI_DIR)/main_window.py
+all: $(UI_DIR)/main_window.py $(UI_DIR)/main_widget.py $(UI_DIR)/track_details.py
 
 $(UI_DIR)/%.py: $(UI_DIR)/%.ui
 	pyuic5 $< -o $@
--- collectionmanager/app.py
+++ collectionmanager/app.py
@@ -9,21 +9,47 @@ import sys
 import collectionmanager.database as database
 import collectionmanager.models as models
 import collectionmanager.ui.main_window as main_window
+import collectionmanager.ui.main_widget as main_widget
+
+
+class MainWidget(QtWidgets.QWidget, main_widget.Ui_Form):
+    """The main application widget
+    """
+    def __init__(self, parent, db):
+        """Constructor for the main application window.
+
+        :param parent: The parent window.
+        """
+        super(MainWidget, self).__init__(parent)
+
+        self.trackModel = models.TrackModel(self, db)
+
+        self.setupUi()
+
+    def setupUi(self, **kwargs):
+        """Set up the user interface.
+
+        :param kwargs: Keyword arguments.
+        """
+        super(MainWidget, self).setupUi(self)
+
+        self.trackTableView.setModel(self.trackModel)
+        self.trackModel.refresh()
 
 
 class MainWindow(QtWidgets.QMainWindow, main_window.Ui_MainWindow):
-    """
-    The application class
+    """The main application window
     """
     def __init__(self, parent=None):
-        """Constructor for the main application class.
+        """Constructor for the main application window.
 
         :param parent: The parent window.
         """
         super(MainWindow, self).__init__(parent)
 
         self.db = database.Database(QtCore.QStandardPaths.writableLocation(QtCore.QStandardPaths.AppDataLocation))
-        self.trackModel = models.TrackModel(self, self.db)
+        self.mainWidget = MainWidget(self, self.db)
+
         self.setupUi()
 
     def setupUi(self, **kwargs):
@@ -33,13 +59,10 @@ class MainWindow(QtWidgets.QMainWindow, main_window.Ui_MainWindow):
         """
         super(MainWindow, self).setupUi(self)
 
-        self.setCentralWidget(self.trackTableView)
+        self.setCentralWidget(self.mainWidget)
 
         self.fileOpenAction.triggered.connect(self.open_directory)
         self.fileQuitAction.triggered.connect(QtWidgets.qApp.quit)
-
-        self.trackTableView.setModel(self.trackModel)
-        self.trackModel.refresh()
 
     def open_directory(self):
         """Called when the user selects a directory to open.
@@ -48,7 +71,7 @@ class MainWindow(QtWidgets.QMainWindow, main_window.Ui_MainWindow):
         if directory:
             self.db.add_directory(directory)
             self.db.save()
-            self.trackModel.refresh()
+            self.mainWidget.trackModel.refresh()
 
 
 def main():
