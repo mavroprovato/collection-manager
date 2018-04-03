ec535fcd787ae1620210cba72b3fe2c67a9f9335
Kostas Kokkoros <mavroprovato@gmail.com>
2018-04-04 02:34:48 +0300

    Fetch missing album art

---
+++ collectionmanager/albumart.py
@@ -0,0 +1,117 @@
+"""Module to fetch album art.
+"""
+import argparse
+import logging
+import tempfile
+import os
+import re
+import sys
+
+import mutagen
+import mutagen.id3
+import requests
+
+
+class MusicbrainzFetcher:
+    HEADERS = {'User-Agent': 'collection-manager/0.0.1 (https://github.com/mavroprovato/collection-manager)'}
+
+    def __init__(self):
+        self._cache = {}
+
+    def fetch(self, artist: str, album: str):
+        """Fetch the album art for a release.
+
+        :param artist: The artist name.
+        :param album: The album name.
+        """
+        if (artist, album) not in self._cache:
+            release_ids = self.get_release_ids(artist, album)
+            album_art_list = []
+            for release_id in release_ids:
+                album_art = self.get_album_art(release_id)
+                if album_art is not None:
+                    album_art_list.append(album_art)
+            self._cache[(artist, album)] = album_art_list
+
+        return self._cache[(artist, album)]
+
+    @staticmethod
+    def get_release_ids(artist, album):
+        """Search for the ids of a release.
+
+        :param artist: The release artist.
+        :param album: The release album.
+        :return: A list with the release ids.
+        """
+        artist = re.sub(r'[:/()?]', '', artist)
+        album = re.sub(r'[:/()?]', '', album)
+        logging.info('Searching for release with artist "%s" and album "%s"', artist, album)
+        response = requests.get('http://musicbrainz.org/ws/2/release-group/', params={
+            'query': 'release:{} AND artist:{}'.format(album, artist), 'fmt': 'json'
+        }, headers=MusicbrainzFetcher.HEADERS)
+        response.raise_for_status()
+
+        data = response.json()
+        if data['release-groups']:
+            logging.info('Found %d release(s).', len(data['release-groups']))
+            return [x['id'] for x in data['release-groups']]
+        else:
+            logging.warning('Could not find release')
+            return []
+
+    @staticmethod
+    def get_album_art(release_id):
+        """Get the album art for a release.
+
+        :param release_id: The release id.
+        :return: A list with all available album art content.
+        """
+        logging.info('Searching cover for release with id: %s', release_id)
+        url = "http://coverartarchive.org/release-group/" + release_id
+        response = requests.get(url, headers=MusicbrainzFetcher.HEADERS)
+        if response.status_code == requests.codes.not_found:
+            logging.warning('Could not find cover')
+            return
+        else:
+            response.raise_for_status()
+
+        data = response.json()
+        logging.info('Found %d cover(s).', len(data['images']))
+
+        url = data['images'][0]['thumbnails']['large']
+        response = requests.get(url, headers=MusicbrainzFetcher.HEADERS)
+        response.raise_for_status()
+
+        return response.content
+
+
+def main():
+    """Main entry point of the script.
+    """
+    # Configure logging
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+    # Parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("scan_dir", help="The directory to scan for files.")
+    args = parser.parse_args()
+
+    # Scan the input directory
+    fetcher = MusicbrainzFetcher()
+    for current_root_name, _, files in os.walk(args.scan_dir):
+        for file_name in files:
+            file_path = os.path.join(current_root_name, file_name)
+            file_info = mutagen.File(file_path)
+            artist, album = file_info['TPE1'][0], file_info['TALB'][0]
+            if 'APIC' not in file_info:
+                logging.info('Album art missing for file %s', file_path)
+                album_art_list = fetcher.fetch(artist, album)
+                if len(album_art_list) == 0:
+                    continue
+                album_art = album_art_list[0]
+                file_info['APIC'] = mutagen.id3.APIC(encoding=3, mime='image/jpeg', type=3, data=album_art)
+                file_info.save()
+
+
+if __name__ == '__main__':
+    main()
