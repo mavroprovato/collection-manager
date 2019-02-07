36eb16336fc7c14f109aad8e00d37fce42367085
Kostas Kokkoros <mavroprovato@gmail.com>
2019-02-07 02:40:31 +0200

    Make the dialog more object oriented

--- collectionmanager/dialogs.py
+++ collectionmanager/dialogs.py
@@ -18,6 +18,7 @@ class TrackDetailDialog(QtWidgets.QDialog, track_details.Ui_Dialog):
         super(TrackDetailDialog, self).__init__(parent)
 
         self.setupUi()
+        self.track_info = None
 
     def setupUi(self, **kwargs):
         """Set up the user interface.
@@ -33,68 +34,67 @@ class TrackDetailDialog(QtWidgets.QDialog, track_details.Ui_Dialog):
 
         :param file_path: The file path.
         """
-        info = track_info.TrackInfo(file_path)
+        self.track_info = track_info.TrackInfo(file_path)
 
-        self.summaryLabel.setText(self.get_track_summary(info))
+        self.summaryLabel.setText(self.get_track_summary())
 
-        self.nameLineEdit.setText(info.name)
-        self.artistLineEdit.setText(info.artist)
-        self.albumArtistEdit.setText(info.album_artist)
-        self.albumLineEdit.setText(info.album)
-        self.yearLineEdit.setText(info.year)
-        self.trackNumberLineEdit.setText(info.track_number)
-        self.diskNumberLineEdit.setText(info.disk_number)
+        self.nameLineEdit.setText(self.track_info.name)
+        self.artistLineEdit.setText(self.track_info.artist)
+        self.albumArtistEdit.setText(self.track_info.album_artist)
+        self.albumLineEdit.setText(self.track_info.album)
+        self.yearLineEdit.setText(self.track_info.year)
+        self.trackNumberLineEdit.setText(self.track_info.track_number)
+        self.diskNumberLineEdit.setText(self.track_info.disk_number)
 
-        self.fileInfoLabel.setText(self.get_file_info_text(info))
+        self.fileInfoLabel.setText(self.get_file_info_text())
 
-    @staticmethod
-    def get_track_summary(info):
+    def get_track_summary(self):
         """Return the track summary from the file info.
 
-        :param info: The file info
         :return: The track summary as a string.
         """
         summary = ""
-        if info.name:
-            summary = "<b>{}</b>".format(info.name)
-        else:
-            return summary
-        if info.artist:
-            summary += " by <b>{}</b>".format(info.artist)
-        else:
-            return summary
-        if info.album:
-            summary += " on <b>{}</b>".format(info.album)
+        if self.track_info:
+            if self.track_info.name:
+                summary = "<b>{}</b>".format(self.track_info.name)
+            else:
+                return summary
+            if self.track_info.artist:
+                summary += " by <b>{}</b>".format(self.track_info.artist)
+            else:
+                return summary
+            if self.track_info.album:
+                summary += " on <b>{}</b>".format(self.track_info.album)
 
         return summary
 
-    @staticmethod
-    def get_file_info_text(info):
+    def get_file_info_text(self):
         """Returns the file info text to be displayed in the file info label
 
-        :param info: The file info.
         :return: The file info text.
         """
-        timedelta_str = str(datetime.timedelta(seconds=round(info.info.length)))
-        if timedelta_str.startswith("0:"):
-            timedelta_str = timedelta_str[2:]
-        return """
-            <table>
-                <tr><td><b>Length:</b></td><td>{}</td>
-                <tr><td><b>Bit rate:</b></td><td>{} Kbps</td>
-                <tr><td><b>Bit rate mode:</b></td><td>{}</td>
-                <tr><td><b>Sample rate:</b></td><td>{}</td>
-                <tr><td><b>Encoder:</b></td><td>{}</td>
-            </table>
-        """.format(
-            timedelta_str,
-            round(info.info.bitrate / 1000),
-            {
-                mutagen.mp3.BitrateMode.UNKNOWN: 'Unknown',
-                mutagen.mp3.BitrateMode.CBR: 'Constant Bitrate',
-                mutagen.mp3.BitrateMode.VBR: 'Variable Bitrate',
-                mutagen.mp3.BitrateMode.ABR: 'Average Bitrate',
-            }[info.info.bitrate_mode],
-            info.info.sample_rate,
-            info.info.encoder_info
-        )
+        if self.track_info:
+            timedelta_str = str(datetime.timedelta(seconds=round(self.track_info.info.length)))
+            if timedelta_str.startswith("0:"):
+                timedelta_str = timedelta_str[2:]
+
+            return """
+                <table>
+                    <tr><td><b>Length:</b></td><td>{}</td>
+                    <tr><td><b>Bit rate:</b></td><td>{} Kbps</td>
+                    <tr><td><b>Bit rate mode:</b></td><td>{}</td>
+                    <tr><td><b>Sample rate:</b></td><td>{}</td>
+                    <tr><td><b>Encoder:</b></td><td>{}</td>
+                </table>
+            """.format(
+                timedelta_str,
+                round(self.track_info.info.bitrate / 1000),
+                {
+                    mutagen.mp3.BitrateMode.UNKNOWN: 'Unknown',
+                    mutagen.mp3.BitrateMode.CBR: 'Constant Bitrate',
+                    mutagen.mp3.BitrateMode.VBR: 'Variable Bitrate',
+                    mutagen.mp3.BitrateMode.ABR: 'Average Bitrate',
+                }[self.track_info.info.bitrate_mode],
+                self.track_info.info.sample_rate,
+                self.track_info.info.encoder_info
+            )
