d778f2207a35a855ea53d04858ae24be06fc070d
Kostas Kokkoros <mavroprovato@gmail.com>
2023-01-30 22:51:16 +0200

    Check file naming (#WIP)

--- collectionmanager/check.py
+++ collectionmanager/check.py
@@ -16,7 +16,9 @@ UNSAFE_CHARACTERS = re.compile(r'[<>:"/\\|?*]')
 UNSAFE_CHARACTERS_REPLACE = '_'
 # The file name pattern
 FILE_NAME_PATTERN = re.compile(
-    r'(?P<artist>[^/]+)/\[(?P<year>[0-9]{4})] (?P<album>[^/]+)/(?P<track_number>[0-9\-]+)\. (.+).(?P<type>mp3|flac)')
+    r'(?P<artist>[^/]+)/'
+    r'\[(?P<year>[0-9]{4})] (?P<album>[^/]+)/'
+    r'(?P<disk_number>[0-9]?)-?(?P<track_number>[0-9]{2})\. (?P<track_name>.+).(?P<type>mp3|flac)')
 
 
 def check_capitalisation(name: str) -> bool:
@@ -31,6 +33,15 @@ def check_capitalisation(name: str) -> bool:
                 return False
 
     return True
+
+
+def replace_unsafe_chars(string: str) -> str:
+    """Replace unsafe characters in a string.
+
+    :param string: The string.
+    :return: The string after replacements.
+    """
+    return UNSAFE_CHARACTERS.sub(UNSAFE_CHARACTERS_REPLACE, string)
 
 
 def check_file(scan_dir: pathlib.Path, file: pathlib.Path):
@@ -73,8 +84,39 @@ def check_file(scan_dir: pathlib.Path, file: pathlib.Path):
         logger.warning("Track title info capitalization is wrong for file '%s': %s", file, track_info.title)
 
     # Check album art
-    if not track_info.album_art:
-        logger.warning("Album art is missing for file '%s", file)
+    # if not track_info.album_art:
+    #     logger.warning("Album art is missing for file '%s", file)
+
+    # Check naming conventions
+    relative_file_name = str(file.relative_to(file.parent.parent.parent))
+    match = FILE_NAME_PATTERN.match(relative_file_name)
+    if match:
+        # Check track year
+        if track_info.year and int(match.group('year')) != track_info.year:
+            logger.warning("File name year does not match file info for file '%s', should be '%s', is '%s'",
+                           file, track_info.year, match.group('year'))
+        # Check track album
+        if track_info.album and match.group('album') != replace_unsafe_chars(track_info.album):
+            logger.warning("File name album does not match file info for file '%s', should be '%s', is '%s'",
+                           file, track_info.album, match.group('album'))
+        # Check disk number
+        if match.group('disk_number'):
+            if not track_info.disk_number or int(match.group('disk_number')) != track_info.disk_number:
+                logger.warning("File name disk number does not match file info for file '%s', should be '%s', is '%s'",
+                               file, track_info.disk_number, match.group('disk_number'))
+
+        # Check track number
+        if track_info.number and int(match.group('track_number')) != track_info.number:
+            logger.warning("File name track number does not match file info for file '%s', should be '%s', is '%s'",
+                           file, track_info.number, match.group('track_number'))
+
+        # TODO: replace this with a better way to check for compilations
+        if track_info.album_artist != 'Various Artists' and match.group('artist') != 'Various Artists':
+            if track_info.artist and match.group('artist') != replace_unsafe_chars(track_info.artist):
+                logger.warning("File name artist does not match file info for file '%s', should be '%s', is '%s'",
+                               file, track_info.artist, match.group('artist'))
+    else:
+        logger.warning("File name '%s' does not match naming conventions", file)
 
 
 def main():
